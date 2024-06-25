8b9ee0b2cafa7a760beb4a6a36928a2f37687f38
goli01 <priya.goli@cancer.org.uk>
2024-06-25 16:29:31 +0100

    Fixed the locator for page assertion

--- tests/createnewtrial.spec.ts
+++ tests/createnewtrial.spec.ts
@@ -10,7 +10,9 @@ test.only("create new trial", async ({ page }) => {
     //let url = process.env.URL ?? '';
    // await page.goto('url');
     await page.goto('https://test-trialsconnect.crm11.dynamics.com/');
-    await expect(page.getByText('Active Trials')).toBeVisible();
+    //await page.getByText('Active Trials').first().waitFor();
+    //await expect(page.getByText('TrialsConnect Number')).toBeVisible();
+    await page.locator('[data-testid="cruk_pic_trials_connect_number"]').isVisible();
     await page.locator((CreateNewTrialSelectors.NewTrialSelector)).click();
     await page.locator(CreateNewTrialSelectors.TrialTitleSelector).click();
     await page.locator(CreateNewTrialSelectors.TrialTitleSelector).fill('sample test');
